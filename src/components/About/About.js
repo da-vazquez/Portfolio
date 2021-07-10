aa57ae592231c8b4ab93fd5c534bd338d5cf20bf
da-vazquez <da.vazquez@yahoo.com>
2021-07-09 17:17:29 -0700

    hero-section buttons added, improved styling overall especially about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import './About.css';
 import AOS from 'aos';
-import 'aos/dist/aos.css'
-AOS.init();
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+AOS.init({
+  duration: 1200,
+  easing: 'ease-in-out-back'
+})
+
 
 const About = () => (
-  <div data-aos="fade" data-aos-easing="ease-in-out" className="container">
-    <div className='box1'>
+  <div className="container">
+    <div className='box-left'>
+        <h3>About</h3>
+        <p>I'm a Full Stack Web Developer from Phoenix, Arizona. 
+        I like dabbling in various parts of front and backend development and I'm always eager to learn new technologies. 
+        In my free time I enjoy gaming and I'm also an avid basketball fan. Go Suns!</p>
+        <br/>
+        <h3>Skills</h3>
+        <p>On the Frontend, I specialize in building dynamic and performant UI's using HTML, CSS, React and various other libraries such as Bootstrap, Material-UI and Spring. I ensure that my Web Apps are fully Accessible and follow Symantic HTML principles so they may enjoyed on any device. On the backend I use a MERN stack to
+          build and manage RESTFUL API's that are compliant to the client's specifications and easily consumed by the frontend Web App.</p>    
     </div>
-    <div className='box2'>Box 2</div>
-    <div className='box3'>Box 3</div>
-    <div className='box4'>Box 4</div>
-    <div className='box5'>Box 5</div>
-    <div className='sidebar'>about me.</div>
-    
+    <div className='box-right'>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-js-square"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-react"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-node"></i>
+        <i data-aos="slide-left" data-aos-once="true"  class="fab fa-github"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fas fa-database"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-html5"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-css3-alt"></i>
+        <i data-aos="slide-left" data-aos-once="true" class="fab fa-python"></i>
+    </div>
   </div>
 );
 
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -11,6 +11,9 @@ const HeroSection = () => {
       <h2 className="hero-main-text">Daniel Vazquez</h2>
       <h4 className="hero-sub-text">Full Stack Web Developer</h4>
       <div className='hero-btns'>
+        <button className="normal">Learn more</button>
+        <button className="danger">Contact Now<span><i class="fas fa-mouse-pointer"></i></span></button>
+
       </div>
     </div>
   );
