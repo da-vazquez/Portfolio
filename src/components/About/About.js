b2d8ded6f04ec68ccd1ff44e23c1dcb94ce38833
Daniel V <71048973+da-vazquez@users.noreply.github.com>
2021-09-25 03:07:59 -0700

    Contact section updated (#4)
    * updated URL for scribble stadium project
    
    * formik removed and replaced with useState, logic updated
    
    * formik uninstalled, no longer using
    
    * updated bio paragraph
    
    * whitespace removed
    
    * formik uninstalled

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -23,9 +23,8 @@ const About = () => (
   <div className="about-grid" id="about">
     <div className='box-left'>
         <h3>About</h3>
-        <p>I'm a Full Stack Web Developer from Phoenix, Arizona. 
-        I like dabbling in various parts of front and backend development and I'm always eager to learn new technologies. 
-        In my free time I enjoy gaming and I'm also an avid basketball fan. Go Suns!</p>
+        <p>My name is Daniel! I'm a Full Stack Web Developer from Phoenix, AZ. I love to dabble in various frontend and backend code and I'm always on the hunt for the latest and greatest technologies. I have a passion for tech and I strive to make the world a greater place by providing people with cutting-edge products that enrich their lives. I'm always looking to collaborate, 
+          so please get in touch with me if you think I would be a great addition to your team</p>
         <br/>
         <h3>Skills</h3>
         <p>On the Frontend, I specialize in building dynamic and performant UI's using HTML, CSS, React and various other libraries such as Bootstrap, Material-UI and Spring. I ensure that my Web Apps are fully Accessible and follow Symantic HTML principles so they may enjoyed on any device. On the backend I use a MERN stack to
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
     <div className="projects-container" id="projects">
       <div className="projects-grid">
         <div className="project-item-1">
-          <a href="https://github.com/da-vazquez/Portfolio" target="_blank" rel="noopener noreferrer">Scribble Stadium</a>
+          <a href="https://github.com/da-vazquez/scribble-stadium-fe" target="_blank" rel="noopener noreferrer">Scribble Stadium</a>
         </div>
         <div className="project-item-2">
           <a href="https://github.com/Water-My-Plants-PTCT/plants-backend" target="_blank" rel="noopener noreferrer">Water My Plants</a>
