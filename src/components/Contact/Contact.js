89bf1843984bd876cb4b3698fabc4a36a5dc86f1
da-vazquez <da.vazquez@yahoo.com>
2021-09-25 03:01:30 -0700

    formik removed and replaced with useState, logic updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useFormik } from 'formik';
+import React, {useState} from 'react';
 import "./Contact.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -10,110 +9,91 @@ AOS.init({
   easing: 'ease-in-out-back'
 })
 
-const validate = values => {
-  const errors = {};
+const initialValues = {name: '', phone: '', email: '', message: ''}
  
-    if (!values.username) {
-    errors.username = 'username required';
-    } else if (values.username.length < 6) {
-    errors.username = 'Username must be at least 6 characters';
-    }
- 
-    if (!values.password) {
-      errors.password = 'password required';
-    } else if (values.password.length < 6) {
-      errors.password = 'Password must be at least 6 characters';
-    }
- 
-   return errors;
- };
- 
-const ContactForm = (props) => {
+const ContactForm = () => {
+  const [values, setValues] = useState(initialValues)
+  const [sent, setSent] = useState("Send")
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      phone: '',
-      email: '',
-      message: ''
-     }
-     ,
-      validate,
-      
-      onSubmit: async(values) => {
-        console.log(values)
-        FormReset();
-     }}
-     )
-
-     const FormReset = () => {
-      Array.from(document.querySelectorAll("input")).forEach(
-        input => (input.value = ""))
-      
+  const handleChange = (e) => {
+    e.preventDefault()
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {"Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(values),
+        });
+        setSent("Message Sent!");
+        
+        let result = await response.json();
+        console.log(result.status)
+    }    
+    catch (error) {
+      console.log("unable to send message", error.message)
     }
-
-    /* {formik.errors.username ? <div style={{color: 'red'}}>{formik.errors.name}</div> : null}
-    {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.phone}</div> : null}
-    {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
-    {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.message}</div> : null} */
+  }
    
-    return (
-      <div className="contact-section" id="contact">
-      
-      <div className= 'contact-form'>
-        <form onSubmit={formik.handleSubmit}>
-        <label className="form-label">Name:</label>
-        <input
-          id='name'
-          placeholder='enter name'
-          name='name'
-          type='text'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.name}
-        />
+  return (
+    <div className="contact-section" id="contact">
+    <div className= 'contact-form'>
+      <form onSubmit={handleSubmit}>
+      <label className="form-label">Name:</label>
+      <input
+        id='name'
+        placeholder='enter name'
+        name='name'
+        type='text'
+        onChange={handleChange}
+        value={values.name}
+      />
  
-        <label className="form-label">Phone Number:</label>
-        <input
-          id='phone'
-          placeholder='enter phone number'
-          name='phone'
-          type='text'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.phone}
-        />
+      <label className="form-label">Phone Number:</label>
+      <input
+        id='phone'
+        placeholder='enter phone number'
+        name='phone'
+        type='text'
+        onChange={handleChange}
+        value={values.phone}
+      />
 
-        <label className="form-label">Email:</label>
-        <input
-          id='email'
-          placeholder='enter email address'
-          name='email'
-          type='email'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
+      <label className="form-label">Email:</label>
+      <input
+        id='email'
+        placeholder='enter email address'
+        name='email'
+        type='email'
+        onChange={handleChange}
+        value={values.email}
+      />
 
-        <label className="form-label">Message:</label>
-        <input
-          id='message'
-          placeholder='enter message'
-          name='message'
-          type='text'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.message}
-        />
+      <label className="form-label">Message:</label>
+      <input
+        id='message'
+        placeholder='enter message'
+        name='message'
+        type='text'
+        onChange={handleChange}
+        value={values.message}
+      />
 
-      <button className='submit-button' type='submit'>Submit</button>
+      <button className='submit-button' type='submit'>{sent}</button>
     </form>
   </div>
 
     <div className='contact-text'>
-      <div className='text-items'>
-        <div className="fa-hand-point-right" data-aos="slide-right" data-aos-once="true"><FaHandPointRight/></div>
-        <h2>I'd love to hear from you</h2>
+    <div className='text-items'>
+    <div className="fa-hand-point-right" data-aos="slide-right" data-aos-once="true"><FaHandPointRight/></div>
+      <h2>I'd love to hear from you</h2>
       </div>
     </div>
   </div>
