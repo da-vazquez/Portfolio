96d619e360472139ece1ddb361a5d3473418f239
Daniel Vazquez <da.vazquez728@gmail.com>
2021-09-28 00:45:06 -0700

    icons and links added for social accounts

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import "./footer.css";
+import { 
+  FaGithub,
+  FaLinkedin,
+  FaMedium
+ } from 'react-icons/fa';
 
 const Footer = () => {
 
@@ -7,6 +12,14 @@ const Footer = () => {
   <div className="footer">
     <div className="footer-content">
       <p><strong>&copy;</strong>Daniel Vazquez 2021, All rights reserved.</p>
+      <div className="footer-icons-wrapper">
+        <a href="https://github.com/da-vazquez" target="_blank" rel="noopener noreferrer">
+          <FaGithub id="github"/></a>
+        <a href="https://www.linkedin.com/in/da-vazquez/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin id="linkedin"/></a>
+        <a href="https://medium.com/@da.vazquez728" target="_blank" rel="noopener noreferrer">
+          <FaMedium id="medium"/></a>   
+      </div>
     </div>
   </div>
   )
