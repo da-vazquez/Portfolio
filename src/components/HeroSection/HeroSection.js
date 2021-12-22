4fda6d4af1df60d05df73dc36e14fa1c5d1c3294
Daniel Vazquez <da.vazquez728@gmail.com>
2021-12-22 02:40:04 -0700

    typewriter effect added and new bg image for hero container

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './HeroSection.css';
+
 import { FaMousePointer } from 'react-icons/fa';
+import Typewriter from 'typewriter-effect';
+
 
 const HeroSection = () => {
 
@@ -15,7 +18,14 @@ const HeroSection = () => {
   return (
     <div className='hero-container' id='home'>
       <h2>Daniel Vazquez</h2>
-      <h4>Full Stack Web Developer</h4>
+      <Typewriter
+        options={{
+          strings: ['Full Stack Web Developer'],
+          autoStart: true,
+          loop: true,
+          pauseFor: 30000
+        }}
+      />
       <div className='hero-btns'>
         <button onClick={handleLearnMore} className='normal'>Learn more</button>
         <button onClick={handleContactNow} className='danger'>Contact Now<span>
