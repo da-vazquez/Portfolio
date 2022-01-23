576995bdf308add1458a88489aa38b0cfbd9359b
Daniel Vazquez <da.vazquez728@gmail.com>
2022-01-23 00:48:17 -0700

    about button removed for simplicity

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HeroSection.css';
 
 import { FaMousePointer } from 'react-icons/fa';
 import Typewriter from 'typewriter-effect';
+import Profile from "./profile-picture.png";
 
 
 const HeroSection = () => {
+  const [mobile, setMobile] = useState(false)
 
-  const handleLearnMore = () => {
-    window.location.hash = 'about' 
-  }
+  const styleHandler = () => {
+    if (window.innerWidth > 650) {
+      setMobile(false);
+    } else {
+      setMobile(true);
+    }
+  };
 
   const handleContactNow = () => {
     window.location.hash = 'contact'
@@ -17,17 +23,19 @@ const HeroSection = () => {
 
   return (
     <div className='hero-container' id='home'>
+      <div className='profile-picture'>
+        <img src={Profile} alt='developer portfolio'/>
+      </div>
       <h2>Daniel Vazquez</h2>
       <Typewriter
         options={{
-          strings: ['Full Stack Web Developer'],
+          strings: [`Software Engineer`],
           autoStart: true,
           loop: true,
           pauseFor: 30000
         }}
       />
       <div className='hero-btns'>
-        <button onClick={handleLearnMore} className='normal'>Learn more</button>
         <button onClick={handleContactNow} className='danger'>Contact Now<span>
           <FaMousePointer className='fa-mouse-pointer'/></span></button>
       </div>
