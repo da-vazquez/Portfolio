7dcd99ddf57883694d7e0b5c20f4ff968dfebb7d
Daniel Vazquez <da.vazquez728@gmail.com>
2022-08-20 22:37:47 -0700

    apm-server config for analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withTransaction } from '@elastic/apm-rum-react'
 import "./App.css";
 //components
 import Nav from './components/Nav/Nav';
@@ -9,6 +10,14 @@ import Grabber from './components/Grabber/Grabber';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/footer';
 
+import { init as initApm } from '@elastic/apm-rum'
+
+const apm = initApm({
+  serviceName: 'portfolio-dv',
+  serverUrl: process.env.REACT_APP_APM_SERVER_URL,
+  serviceVersion: '1.0',
+  active: true
+})
 
 function App() {
   return (
@@ -24,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+export default withTransaction('AppComponent', 'component')(App);
