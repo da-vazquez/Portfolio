34a3f722a4fae1fb25f8c636e3407eea5279d1b4
Daniel Vazquez <da.vazquez728@gmail.com>
2022-08-30 02:11:08 -0700

    fixing routes

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "Swithcing to branch main"
+git checkout main
+
+echo "Building app..."
+npm run build
+
+echo "Deploying files to server..."
+scp -r build/* dvazque7@45.79.228.103:/var/www/vaztech.co/
+
+echo "Done!"
--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { ApmRoute } from '@elastic/apm-rum-react'
-import { Redirect } from 'react-router-dom';
+// import { ApmRoute } from '@elastic/apm-rum-react'
+// import { Redirect } from 'react-router-dom';
 import "./App.css";
 //components
 import Home from './components/Home/Home';
 
-import { init as initApm } from '@elastic/apm-rum'
+// import { init as initApm } from '@elastic/apm-rum'
 
-const apm = initApm({
-  serviceName: 'portfolio-dv',
-  serverUrl: process.env.REACT_APP_APM_SERVER_URL,
-  serviceVersion: '1.0',
-  environment: 'production',
-  active: true
-})
+// const apm = initApm({
+//   serviceName: 'portfolio-dv',
+//   serverUrl: process.env.REACT_APP_APM_SERVER_URL,
+//   serviceVersion: '1.0',
+//   environment: 'production',
+//   active: true
+// })
 
 function App() {
   return (
     <div className='App'>
-      <ApmRoute
-          exact
-          path="/"
-          component={() => (
-            <Redirect to={{pathname: '/home'}}/>
-          )}
-        />
-        <ApmRoute path="/home" component={Home}/>
+      <Home/>
   </div>
   );
 }
