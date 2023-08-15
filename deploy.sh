44edbf030a578a9988776c1542bc3366dc339711
Daniel V <71048973+da-vazquez@users.noreply.github.com>
2023-08-15 01:13:30 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* dvazque7@45.79.228.103:/var/www/vaztech.co/
+scp -r build/* {process.env.SERVER_USERS}@{process.env.SERVER_HOST}:/var/www/vaztech.co/
 
 echo "Done!"
